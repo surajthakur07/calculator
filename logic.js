1417a2fd857f36f64b1e208ce01d498c7510bf6f
Mahi Jain <Mahi Jain>
2021-04-22 17:40:19 +0530

    v1

---
+++ logic.js
@@ -0,0 +1,30 @@
+let screen = document.getElementById("screen");
+let screenValue = "";
+buttons = document.querySelectorAll("button");
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    let buttonText = e.target.innerText;
+    console.log("Button text is", buttonText);
+    if (buttonText == "x") {
+      buttonText = "*";
+      screenValue += buttonText;
+      screen.innerText = screenValue;
+    } else if (buttonText == "AC") {
+      screenValue = "";
+      screen.innerText = screenValue;
+    } else if (buttonText == "=") {
+      screenValue = eval(screenValue);
+      screen.innerText = screenValue;
+    } else if (buttonText == "÷") {
+      buttonText = "/";
+      screenValue += buttonText;
+      screen.innerText = screenValue;
+    } else if (buttonText == "C") {
+      screenValue = screenValue.slice(0, buttonText.length - 2);
+      screen.innerText = screenValue;
+    } else {
+      screenValue += buttonText;
+      screen.innerText = screenValue;
+    }
+  });
+}
